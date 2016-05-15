85532eeafb6ece0babcd7907ef3d8f0120c3cf3e
Jason Bolaños <jason.bolanos@janeirodigital.com>
2016-05-15 08:41:41 -0600

    Adding tomdoc to PersonStatusNotifier service

--- app/services/person_status_notifier.rb
+++ app/services/person_status_notifier.rb
@@ -7,10 +7,28 @@
 # #=> true
 class PersonStatusNotifier
 
+  #Public constructor of class
+  #
+  #Examples
+  #
+  # PersonStatusNotifier.new
+  # #=> #<PersonStatusNotifier:0x00000004554b60>
+  #
+  #Returns PersonStatusNotifier object
   def initialize
     @people_ids = Person.pluck(:id)
   end
 
+  #Public enqueues a job for each Person of the system excluding the new one for email sending
+  #
+  #new_person_id - id of created person
+  #
+  #Examples
+  #
+  # notify_person_creation(1)
+  # #=> true
+  #
+  #Returns TrueClass
   def notify_person_creation(new_person_id)
     @people_ids.each do |person_id|
       if person_id != new_person_id
@@ -20,6 +38,16 @@ class PersonStatusNotifier
     true
   end
 
+  #Public enqueues a job for each Person of the system to notify a person deletion
+  #
+  #deleted_person_id - id of deleted person
+  #
+  #Examples
+  #
+  # notify_person_deletion(1)
+  # #=> true
+  #
+  #Returns TrueClass
   def notify_person_deletion(deleted_person_id)
     @people_ids.each do |person_id|
       Resque.enqueue(PersonMailerJob, person_id, deleted_person_id, :deleted_person_email)
