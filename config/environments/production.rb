8f711e27c160f5c2d3afe501f2771bef503de0b0
Jason Bolaños <jason.bolanos@janeirodigital.com>
2016-05-15 20:58:49 -0600

    Production Mailer configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,6 +68,9 @@ Rails.application.configure do
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = Rails.application.secrets[:mailer]['settings'].symbolize_keys
+
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
